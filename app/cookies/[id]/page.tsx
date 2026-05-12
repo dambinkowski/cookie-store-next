9ddf593b7c72f23ed250a71194f84c8f16c4c2c0
Damian Binkowski <dambinkowski@gmail.com>
2026-05-12 11:40:59 -0400

    created shared lib move cookies data there

--- app/cookies/[id]/page.tsx
+++ app/cookies/[id]/page.tsx
@@ -1,11 +1,6 @@
 import Link from "next/link";
+import { cookies } from "@/lib/cookies"
 
-const cookies = [
-  { id: 1, name: "Chocolate Chip", price: 2.5 },
-  { id: 2, name: "Oatmeal Raisin", price: 2.0 },
-  { id: 3, name: "Peanut Butter", price: 2.5 },
-  { id: 4, name: "Snickerdoodle", price: 2.0 },
-];
 
 export default async function CookiePage(props: PageProps<"/cookies/[id]">) {
   const { id } = await props.params;
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,11 +1,5 @@
 import Link from "next/link";
-
-const cookies = [
-  { id: 1, name: "Chocolate Chip", price: 2.5 },
-  { id: 2, name: "Oatmeal Raisin", price: 2.0 },
-  { id: 3, name: "Peanut Butter", price: 2.5 },
-  { id: 4, name: "Snickerdoodle", price: 2.0 },
-];
+import { cookies } from "@/lib/cookies"
 
 export default function Menu() {
   return (
